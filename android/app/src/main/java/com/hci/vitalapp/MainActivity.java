c20fef3874e461103b0ccfaa1e56ecacf77725ba
Javier Sarmiento <javiersarmiento02@gmail.com>
2023-07-08 00:22:24 -0500

    upload Android native code folder

---
+++ android/app/src/main/java/com/hci/vitalapp/MainActivity.java
@@ -0,0 +1,5 @@
+package com.hci.vitalapp;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'VitalApp',
+  appId: 'com.hci.vitalapp',
+  appName: 'Vital',
   webDir: 'www',
   server: {
     androidScheme: 'https'
