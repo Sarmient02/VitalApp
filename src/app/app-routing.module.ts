67b8662233016aeb714baeea0640c03f9e7c5ba4
Javier Sarmiento <javiersarmiento02@gmail.com>
2023-07-11 19:30:58 -0500

    fix button appearance

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  // {
+  //   path: '',
+  //   loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  // },
   {
     path: '',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/main-user/main-tabs/main-tabs.module').then( m => m.MainTabsPageModule)
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'main-tabs',
     pathMatch: 'full'
   },
   {
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,18 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../components/header/header.component';
 import { IonicModule } from '@ionic/angular';
+import { AlertButtonComponent } from '../components/alert-button/alert-button.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    
+    AlertButtonComponent
   ],
   imports: [
     IonicModule,
     CommonModule
   ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
+    AlertButtonComponent
   ]
 })
 export class SharedModule { }
